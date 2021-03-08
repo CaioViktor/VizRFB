9a4981502767b5160d28ae525ae41e30a27263ea
CaioViktor <caioviktor@hotmail.com>
2021-03-08 15:36:17 -0300

    situação por porte

--- static/js/estabelecimentos.js
+++ static/js/estabelecimentos.js
@@ -2,6 +2,8 @@ const  path_estabelecimentos = path_data+"estabelecimentos.csv";
 var w = window.innerWidth-100;
 var h = window.innerHeight/2;
 let next,last;
+format = d3.format(".2f")
+
 
 
 d3.csv(path_estabelecimentos).then(function(data){
@@ -14,12 +16,14 @@ d3.csv(path_estabelecimentos).then(function(data){
 	
 	let lineChartQ11 = dc.lineChart("#Q11_linechart");
 	let data_table = dc.dataTable("#table_estabs");
+	let barchart= dc.barChart("#Q3");
+	// let lineChartF = dc.lineChart("#fechados");
 
 
 	let facts = crossfilter(data);
 
 
-
+	//Q11
 	let dim_date_start = facts.dimension(d=>d.date_start);
 	let _group = dim_date_start.group().reduceSum(function(d){return 1});
 	var group_date_start = {
@@ -37,7 +41,7 @@ d3.csv(path_estabelecimentos).then(function(data){
 
 	lineChartQ11.width(w)
             .height(h)
-            .margins({top: 10, right: 10, bottom: 50,left:50})
+ 				.margins({left: 80, top: 20, right: 10, bottom: 40})
             .dimension(dim_date_start)
             .group(group_date_start)
             .x(hourScale)
@@ -52,8 +56,67 @@ d3.csv(path_estabelecimentos).then(function(data){
 
 
 
+
+
+    //Q3
+    let dim_porte = facts.dimension(function(d){ return d.porte});
+    let situacoes = ["Ativa","Baixada","Inapta","Nula"];
+    let group_porte_situacao = dim_porte.group().reduce(function(p,v){
+			//Add
+			p[v.situation] = (p[v.situation] || 0) + 1;
+			return p;
+		},function(p,v){
+			//Remove
+			p[v.situation] = (p[v.situation] || 0) - 1;
+			return p;
+		},function(p,v){
+			//Init
+			return {};
+		});
+		// console.log(candidatosGroup);
+		function sel_stack(i) {
+              return function(d) {
+                	return d.value[i];
+              };
+          	}
+
+    let colorScale = d3.scaleOrdinal()
+                 .domain(["Ativa","Baixada","Inapta","Nula"])
+                 .range(["#4daf4a", "#e41a1c", "#984ea3","#377eb8"])
+
+    barchart.width(768)
+                .height(480)
+                .x(d3.scaleLinear().domain([1, 21]))
+                .margins({left: 80, top: 20, right: 10, bottom: 40})
+                .brushOn(false)
+                .clipPadding(20)
+                .gap(40)
+                .yAxisLabel("Quantidade de estabelecimentos")
+	        	.xAxisLabel("Porte")
+                .title(function (d) {
+                	let total = 0
+                	for (i in d.value){
+                		total += d.value[i];
+                	}
+                	let rate = format((d.value[this.layer]/ total) * 100);
+                    return d.key + '[' + this.layer + ']: ' + d.value[this.layer]+"\n"+rate+"%";
+                })
+                .x(d3.scaleBand())
+            	.xUnits(dc.units.ordinal)
+                .dimension(dim_porte)
+                .group(group_porte_situacao, situacoes[0], sel_stack(situacoes[0]))
+                .renderLabel(true)
+                .colors(colorScale);
+
+    barchart.legend(dc.legend());
+    for (var i = 1; i < 4; ++i) {
+                barchart.stack(group_porte_situacao, '' + situacoes[i], sel_stack(situacoes[i]));
+	}
+
     let dim_cnpj = facts.dimension(d=>d.cnpj_est);
 
+
+    //Datatable
     var ofs = 0, pag = 17;
 
 	function update_offset() {
@@ -109,6 +172,54 @@ d3.csv(path_estabelecimentos).then(function(data){
             .on('preRender', update_offset)
           	.on('preRedraw', update_offset)
           	.on('pretransition', display);
+
+
+
+
+
+ //    //Fechados
+ // 	let dim_fechados_date_start = facts.dimension(d=>d.situationDate);
+
+	// let _group_fechados = dim_fechados_date_start.filter(d=>d.situation == "Baixada").group().reduceSum(function(d){return 1});
+	// var group_fechados_date_start = {
+	// 	all:function () {
+	// 		var cumulate = 0;
+	// 		var g = [];
+	// 		_group_fechados.all().forEach(function(d,i) {
+	// 			cumulate += d.value;
+	// 			g.push({key:d.key,value:cumulate,'in_day':d.value})
+	// 		});
+	// 		return g;
+	// 	}
+	// };
+	// let hourScale_fechados = d3.scaleTime().domain(d3.extent(data,d=> d.situationDate))
+
+	// lineChartF.width(w)
+ //            .height(h)
+ // 			.margins({left: 80, top: 20, right: 10, bottom: 40})
+ //            .dimension(dim_fechados_date_start)
+ //            .group(group_fechados_date_start)
+ //            .x(hourScale_fechados)
+ //            .renderArea(true)
+ //            .yAxisLabel("Quantidade de estabelecimentos fechados")
+	//         .xAxisLabel("Ano de início das atividades")
+	//         .clipPadding(10)
+	//         .elasticY(true)
+	//         .title(function(d) { return 'Dia: ' + d.key+'\nAcumulado: '+ d.value+'\nNo dia: '+d.in_day; })
+ //            .brushOn(false)
+ //            .mouseZoomable(true);
            
+
+
+
+
+
+
+
+
+
+
+
+
   dc.renderAll()
 });
